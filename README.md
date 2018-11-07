5f6c2785939f94e80c71b28efdbe1545c836fbed
bananovitch <piotr.michal.romanowski@gmail.com>
2018-11-07 08:03:58 +0100

    added Readme and dev server

---
+++ README.md
@@ -0,0 +1,8 @@
+# A simple React + Webpack boilerplate
+This is a simple boilerplate to learn React project setup without using Create React App.
+
+What we have here:
+* Babel
+* React + JSX
+* Webpack dev server
+* Dev & Production builds
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom";
+import React from 'react';
 import App from "./App";
+import './main.css';
 
 const arr = [1,2,3];
 const IAmJS6 = () => console.log(...arr);
 window.IAmJS6 = IAmJS6;
 
-ReactDOM.render(<App/>, document.getElementById("app"))
+ReactDOM.render(<App/>, document.getElementById("app"));
